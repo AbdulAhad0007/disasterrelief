ad724c304d92efa0e403b40dc240367afe38f7e2
Abdul Ahad <140952153+AbdulAhad0007@users.noreply.github.com>
2024-09-27 10:09:20 -0700

    updated

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Insert the JavaScript code here
-    // Function to animate the counting up
 const counters = document.querySelectorAll('.count');
-const speed = 200; // Speed of counting
-
-// Function to start counting when the card is in view
+const speed = 200;
 const startCounting = (counter) => {
   const target = +counter.getAttribute('data-target');
   const count = +counter.innerText;
@@ -20,18 +16,15 @@ const startCounting = (counter) => {
 
   updateCount();
 };
-
-// Intersection Observer to trigger the counting when in view
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       startCounting(entry.target);
-      observer.unobserve(entry.target); // Stop observing once the count is done
+      observer.unobserve(entry.target);
     }
   });
-}, { threshold: 0.5 }); // Trigger when 50% of the element is in view
-
-// Apply the observer to each counter
+}, { threshold: 0.5 }); 
+ 
 counters.forEach((counter) => {
   observer.observe(counter);
 });
